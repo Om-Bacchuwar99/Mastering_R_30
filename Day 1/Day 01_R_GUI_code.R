9bf6ee0dc1dc72337de7fc0eb809b1e2858bbb5f
Om Vivek Bacchuwar <105986207+Om-Bacchuwar99@users.noreply.github.com>
2026-01-22 22:11:53 +0000

    Add files via upload

---
+++ Day 1/Day 01_R_GUI_code.R
@@ -0,0 +1,29 @@
+print("hello world")
+
+2+3
+
+#using this for the comment in the code which will not compiled and it will be ignored
+
+#we have now assigned the string below in the variable name demo
+demo<-"this is the string which have been assigned to variable demo"
+demo
+
+#we can even assign numbers and certain operations on the number to a variable
+sum <- 2+3
+sum
+
+#the variable can also be changed/modified in between
+sum <- sum + 5
+sum #look here the value has been changed from 5 to 10
+
+
+#we can even perform operations on the variable and store it in other variable
+sum1 <- sum +10
+sum1
+
+sum1 <- sum1 + 80
+sum1
+
+#now after saving this file and after reopening we have 2 options to run 
+#1 to execute the code line by line by pressing Ctrl + R on each line for the execution
+#2 for running the whole programme at once Edit>Run all
